7ea1aba2376b526b3ed3e5b3b3b4c8478459c458
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-04-13 21:01:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,34 +17,34 @@ selected_time = st.selectbox("Select time duration for Pomodoro session:", [1, 5
 selected_time_seconds = selected_time * 60
 
 button_clicked = st.button("Start", key="start_button")
+stop_checkbox = st.checkbox("Stop", key="stop_checkbox")
 stop_button_clicked = False
-
-stop_button = st.empty()
 
 if button_clicked:
     start_container = st.empty()
     
     with start_container:
-        while selected_time_seconds and not stop_button_clicked:
+        while selected_time_seconds and not stop_button_clicked and not stop_checkbox:
             mins, secs = divmod(selected_time_seconds, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
             st.header(f"⏳ {timer}")
             time.sleep(1)
             selected_time_seconds -= 1
-            if stop_button.button("Stop", key="stop_button"):
+            if stop_checkbox:
                 stop_button_clicked = True
         
-        if not stop_button_clicked:
+        if not stop_button_clicked and not stop_checkbox:
             st.success(f"🔔 {selected_time} minutes is over! Time for a break!")
             time.sleep(2)  # Wait for 2 seconds before starting the break timer
             
             break_time = 5 * 60  # 5 minutes break time
-            while break_time:
+            while break_time and not stop_button_clicked:
                 mins, secs = divmod(break_time, 60)
                 timer = '{:02d}:{:02d}'.format(mins, secs)
                 st.header(f"⏳ {timer}")
                 time.sleep(1)
                 break_time -= 1
             
-            st.error("⏰ 5 minute break is over!")
-            time.sleep(2)  # Wait for 2 seconds before allowing to start another Pomodoro session
+            if not stop_button_clicked:
+                st.error("⏰ 5 minute break is over!")
+                time.sleep(2)  # Wait for 2 seconds before allowing to start another Pomodoro session
