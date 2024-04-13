f2e25dcdd42164ae67488822b654b70462a38670
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-04-13 20:29:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,29 +13,32 @@ st.write("""
 Let's do some focus work in data science with this app.
 """)
 
-# Dropdown for selecting time
-selected_time = st.selectbox("Select time duration for Pomodoro session (in minutes):", [1, 5, 10, 15, 20, 25, 30])
+selected_time = st.selectbox("Select time duration for Pomodoro session:", [1, 5, 10, 15, 20, 25, 30])
 selected_time_seconds = selected_time * 60
 
 button_clicked = st.button("Start")
+stop_button_clicked = False
 
 if button_clicked:
     with st.empty():
-        while selected_time_seconds:
+        while selected_time_seconds and not stop_button_clicked:
             mins, secs = divmod(selected_time_seconds, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
             st.header(f"⏳ {timer}")
             time.sleep(1)
             selected_time_seconds -= 1
             st.success(f"🔔 {selected_time} minutes is over! Time for a break!")
+            if st.button("Stop"):
+                stop_button_clicked = True
 
-    # Break timer
-    break_time = 5 * 60  # 5 minutes break
+    break_time = 5 * 60
     with st.empty():
-        while break_time:
+        while break_time and not stop_button_clicked:
             mins, secs = divmod(break_time, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
             st.header(f"⏳ {timer}")
             time.sleep(1)
             break_time -= 1
             st.error("⏰ 5 minute break is over!")
+            if st.button("Stop"):
+                stop_button_clicked = True
