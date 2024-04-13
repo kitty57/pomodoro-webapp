32a95bd0053dabc0af1d52b2ade6566544c9dcba
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-04-13 20:51:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,29 +17,15 @@ selected_time = st.selectbox("Select time duration for Pomodoro session:", [1, 5
 selected_time_seconds = selected_time * 60
 
 button_clicked = st.button("Start", key="start_button")
-stop_button_clicked = False
 
 if button_clicked:
-    start_container = st.empty()
-    
-    with start_container:
-        while selected_time_seconds and not stop_button_clicked:
+    with st.empty():
+        while selected_time_seconds:
             mins, secs = divmod(selected_time_seconds, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
             st.header(f"⏳ {timer}")
             time.sleep(1)
             selected_time_seconds -= 1
             st.success(f"🔔 {selected_time} minutes is over! Time for a break!")
-            if st.checkbox("Stop", key="stop_checkbox"):
-                stop_button_clicked = True
-    
-    break_time = 5 * 60
-    while break_time and not stop_button_clicked:
-        mins, secs = divmod(break_time, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        st.header(f"⏳ {timer}")
-        time.sleep(1)
-        break_time -= 1
-        st.error("⏰ 5 minute break is over!")
-        if st.checkbox("I don't want break", key="stop_checkbox_break"):
-            stop_button_clicked = True
+
+    st.success("Pomodoro session finished!")
