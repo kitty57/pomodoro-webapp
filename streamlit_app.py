c5dd9171c95bbdd7313f620bea6f486202923b32
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-04-13 20:24:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,41 @@
 import streamlit as st
 import time
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 local_css("styles.css")
+
 st.write("""
 # The Pomodoro App
 
 Let's do some focus work in data science with this app.
-
 """)
+
+# Dropdown for selecting time
+selected_time = st.selectbox("Select time duration for Pomodoro session:", [1, 5, 10, 15, 20, 25, 30])
+selected_time_seconds = selected_time * 60
 
 button_clicked = st.button("Start")
 
-t1 = 1500
-t2 = 300
-
 if button_clicked:
     with st.empty():
-        while t1:
-            mins, secs = divmod(t1, 60)
+        while selected_time_seconds:
+            mins, secs = divmod(selected_time_seconds, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
             st.header(f"⏳ {timer}")
             time.sleep(1)
-            t1 -= 1
-            st.success("🔔 25 minutes is over! Time for a break!")
+            selected_time_seconds -= 1
+            st.success(f"🔔 {selected_time} minutes is over! Time for a break!")
 
+    # Break timer
+    break_time = 5 * 60  # 5 minutes break
     with st.empty():
-        while t2:
-            mins2, secs2 = divmod(t2, 60)
-            timer2 = '{:02d}:{:02d}'.format(mins2, secs2)
-            st.header(f"⏳ {timer2}")
+        while break_time:
+            mins, secs = divmod(break_time, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            st.header(f"⏳ {timer}")
             time.sleep(1)
-            t2 -= 1
+            break_time -= 1
             st.error("⏰ 5 minute break is over!")
